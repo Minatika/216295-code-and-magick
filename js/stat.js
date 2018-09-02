b4e23ee4ebaa81f6e4954b7c5db334dedc7f1dcd
Minata <8minata8@gmail.com>
2018-09-02 20:14:46 +0400

    Изменён экран успешного прохождения уровня:
    - переменные перенесены в объекты;
    - добавлена функция рисования многострочного текста;
    - рисование столбца гистограммы перенесено в функцию;
    - изменен метод поиска максимального элемента в массиве.

--- js/stat.js
+++ js/stat.js
@@ -1,77 +1,113 @@
 'use strict';
 
-var cloudWidth = 420;
-var cloudHeight = 270;
-var cloudX = 100;
-var cloudY = 10;
-var gap = 10;
-var fontGap = 20;
-var textHeight = 80;
-var barHeight = 150;
-var barX = cloudX + fontGap * 2;
-var barY = cloudY + textHeight;
-var columnWidht = 40;
-var columnGap = 50;
+var cloudParams = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  CURVE: 20,
+  X: 100,
+  Y: 10,
+  COLOR: '#ffffff'
+};
+
+var shadowParams = {
+  WIDTH: cloudParams.WIDTH,
+  HEIGHT: cloudParams.HEIGHT,
+  CURVE: cloudParams.CURVE,
+  X: cloudParams.X + 10,
+  Y: cloudParams.Y + 10,
+  COLOR: 'rgba(0, 0, 0, 0.7)'
+};
+
+var textParams = {
+  TEXT: 'Ура вы победили!\nСписок результатов:',
+  FONT: '16px PT Mono',
+  color: '#000000',
+  lineheight: 20,
+  x: cloudParams.X + 20,
+  y: cloudParams.Y + 30,
+  maxWidth: cloudParams.WIDTH - 40,
+  height: 40
+};
+
+var barParams = {
+  HEIGHT: 150,
+  COLUMN_WIDTH: 40,
+  COLUMN_GAP: 50,
+  COLOR_YOUR: 'rgba(255, 0, 0, 1)',
+  x: cloudParams.X + 40
+};
 
 // функция рисования облака
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
+var renderCloud = function (ctx, cloud) {
+  ctx.fillStyle = cloud.COLOR;
   ctx.beginPath();
-  ctx.moveTo(x + fontGap, y);
-  ctx.lineTo(x + cloudWidth - fontGap, y);
-  ctx.quadraticCurveTo(x + cloudWidth, y, x + cloudWidth, y + fontGap);
-  ctx.lineTo(x + cloudWidth, y + cloudHeight - fontGap);
-  ctx.quadraticCurveTo(x + cloudWidth, y + cloudHeight, x + cloudWidth - fontGap, y + cloudHeight);
-  ctx.lineTo(x + fontGap, y + cloudHeight);
-  ctx.quadraticCurveTo(x, y + cloudHeight, x, y + cloudHeight - fontGap);
-  ctx.lineTo(x, y + fontGap);
-  ctx.quadraticCurveTo(x, y, x + fontGap, y);
+  ctx.moveTo(cloud.X + cloud.CURVE, cloud.Y);
+  ctx.lineTo(cloud.X + cloud.WIDTH - cloud.CURVE, cloud.Y);
+  ctx.quadraticCurveTo(cloud.X + cloud.WIDTH, cloud.Y, cloud.X + cloud.WIDTH, cloud.Y + cloud.CURVE);
+  ctx.lineTo(cloud.X + cloud.WIDTH, cloud.Y + cloud.HEIGHT - cloud.CURVE);
+  ctx.quadraticCurveTo(cloud.X + cloud.WIDTH, cloud.Y + cloud.HEIGHT, cloud.X + cloud.WIDTH - cloud.CURVE, cloud.Y + cloud.HEIGHT);
+  ctx.lineTo(cloud.X + cloud.CURVE, cloud.Y + cloud.HEIGHT);
+  ctx.quadraticCurveTo(cloud.X, cloud.Y + cloud.HEIGHT, cloud.X, cloud.Y + cloud.HEIGHT - cloud.CURVE);
+  ctx.lineTo(cloud.X, cloud.Y + cloud.CURVE);
+  ctx.quadraticCurveTo(cloud.X, cloud.Y, cloud.X + cloud.CURVE, cloud.Y);
   ctx.closePath();
   ctx.fill();
 };
 
-// функция поиска максимального элемента
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+// функция рисования текста
+var drawText = function (ctx, text) {
+  var lines = text.TEXT.split('\n');
+  ctx.fillStyle = text.color;
+  ctx.font = text.FONT;
+  for (var i = 0; i < lines.length; i++) {
+    var words = lines[i].split(' ');
+    var line = '';
+    for (var j = 0; j < words.length; j++) {
+      var testLine = line + words[j] + ' ';
+      var testWidth = ctx.measureText(testLine).width;
+      if (testWidth > text.maxWidth) {
+        ctx.fillText(line, text.x, text.y);
+        line = words[j] + ' ';
+        text.y += text.lineHeight;
+        text.height += text.lineHeight;
+      } else {
+        line = testLine;
+      }
     }
+    ctx.fillText(line, text.x, text.y);
+    text.y += text.lineheight;
+    text.height += text.lineheight;
   }
-  return maxElement;
+};
+
+// функция рисования колонки гистограммы
+var drawColumn = function (ctx, bar, name, value, maxValue, number) {
+  var columnOpacity = Math.random();
+  if (columnOpacity < 0.1) {
+    columnOpacity = 0.5;
+  }
+  var columnHeight = Math.round(value * bar.HEIGHT / maxValue);
+  ctx.fillStyle = name === 'Вы' ? bar.COLOR_YOUR : 'rgba(0, 0, 255, ' + columnOpacity + ')';
+  ctx.fillRect(bar.x + number * (bar.COLUMN_GAP + bar.COLUMN_WIDTH), bar.y + bar.HEIGHT - columnHeight, bar.COLUMN_WIDTH, columnHeight);
+  ctx.fillStyle = textParams.color;
+  ctx.fillText(Math.round(value), bar.x + number * (bar.COLUMN_GAP + bar.COLUMN_WIDTH), bar.y + bar.HEIGHT - columnHeight - 10);
+  ctx.fillText(name, bar.x + number * (bar.COLUMN_GAP + bar.COLUMN_WIDTH), bar.y + bar.HEIGHT + 20);
 };
 
 window.renderStatistics = function (ctx, names, times) {
   // рисование облака
-  renderCloud(ctx, cloudX + gap, cloudY + gap, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, cloudX, cloudY, '#ffffff');
+  renderCloud(ctx, shadowParams);
+  renderCloud(ctx, cloudParams);
 
   // рисование текста на облаке
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', cloudX + fontGap, cloudY + fontGap + gap);
-  ctx.fillText('Список результатов:', cloudX + fontGap, cloudY + fontGap * 2 + gap);
+  drawText(ctx, textParams);
 
-  // если массивы times и names разной длины, лишние элементы удаляются
-  if (times.length !== names.length) {
-    var differenceLengths = Math.abs(times.length - names.length);
-    if (times.length > names.length) {
-      times.splice(times.length - differenceLengths);
-    } else {
-      names.splice(names.length - differenceLengths);
-    }
-  }
+  var maxTime = Math.max.apply(Math, times);
 
-  var maxTime = getMaxElement(times);
-
+  // добавляем гистограмме координату Y после того, как известна высота блока с текстом
+  barParams.y = cloudParams.Y + textParams.height;
   // рисование гистограммы
   for (var i = 0; i < times.length; i++) {
-    var columnOpacity = Math.random();
-    var columnHeight = Math.round(times[i] * barHeight / maxTime);
-    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + columnOpacity + ')';
-    ctx.fillRect(barX + i * (columnGap + columnWidht), barY + barHeight - columnHeight, columnWidht, columnHeight);
-    ctx.fillStyle = '#000000';
-    ctx.fillText(Math.round(times[i]), barX + i * (columnGap + columnWidht), barY + barHeight - columnHeight - gap);
-    ctx.fillText(names[i], barX + i * (columnGap + columnWidht), barY + barHeight + fontGap);
+    drawColumn(ctx, barParams, names[i], times[i], maxTime, i);
   }
 };
