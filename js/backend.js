8d98c7653e5ae0cd59c24c8d2fe4902299f261b3
Minata <8minata8@gmail.com>
2018-09-25 19:32:09 +0400

    Создание запроса к серверу вынесено в отдельную функцию.

--- js/backend.js
+++ js/backend.js
@@ -5,8 +5,8 @@
   var URL_LOAD = 'https://js.dump.academy/code-and-magick/data';
   var SUCCESS_STATUS = 200;
 
-  // функция загружает данные с сервера
-  var load = function (onLoad, onError) {
+  // функция создает запрос к серверу
+  var getRequest = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
@@ -26,33 +26,22 @@
     });
 
     xhr.timeout = 10000;
-    xhr.open('GET', URL_LOAD);
-    xhr.send();
+
+    return xhr;
+  };
+
+  // функция загружает данные с сервера
+  var load = function (onLoad, onError) {
+    var request = getRequest(onLoad, onError);
+    request.open('GET', URL_LOAD);
+    request.send();
   };
 
   // функция отправляет данные на сервер
   var save = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      if (xhr.status === SUCCESS_STATUS) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000;
-    xhr.open('POST', URL_SAVE);
-    xhr.send(data);
+    var request = getRequest(onLoad, onError);
+    request.open('POST', URL_SAVE);
+    request.send(data);
   };
 
   // экспортируемый объект
